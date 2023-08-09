e68ce9782cae1535d9cd7e94ecdec94ef62fbef7
feberts <64541491+feberts@users.noreply.github.com>
2023-08-09 18:55:26 +0200

    update

--- generate-website.sh
+++ generate-website.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
 
-echo "generating website..."
+ROOT="./website_root/"
+mkdir "$ROOT"
+echo "hallo 123" > "$ROOT"/hallo.txt
+mkdir "$ROOT"subdir
+echo "bye 123" > "$ROOT"/subdir/bye.txt
+mkdir "$ROOT"/subdir/subsubdir
+echo "ciao 123" > "$ROOT"/subdir/subsubdir/ciao.txt
