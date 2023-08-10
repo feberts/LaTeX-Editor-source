e8a4119f3d99bdc9ec032d367f9f840050092146
feberts <64541491+feberts@users.noreply.github.com>
2023-08-10 19:42:51 +0200

    update

--- generate-website.sh
+++ generate-website.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
 ROOT=./"$1"/
+echo "new website: '$ROOT'"
 echo "hallo abc" > "$ROOT"/hallo.txt
 mkdir "$ROOT"subdir
 echo "bye abc" > "$ROOT"/subdir/bye.txt
 mkdir "$ROOT"/subdir/subsubdir
-echo "ciao abcde" > "$ROOT"/subdir/subsubdir/ciao.txt
+echo "ciao abcdef" > "$ROOT"/subdir/subsubdir/ciao.txt
