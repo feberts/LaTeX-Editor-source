bdd65c099ffa6c8ff4a577ac32755c56d21ac4fa
feberts <64541491+feberts@users.noreply.github.com>
2023-08-11 13:29:30 +0200

    update

--- generate-website.sh
+++ generate-website.sh
@@ -3,8 +3,8 @@
 ROOT="$1"/
 CUSTOM_URL="$2"
 
-echo "$ROOT"
-echo "$CUSTOM_URL"
+# echo "$ROOT"
+# echo "$CUSTOM_URL"
 
 echo "new website: '$ROOT'"
 echo "hallo abc" > "$ROOT"/hallo.txt
@@ -13,5 +13,5 @@ echo "bye abc" > "$ROOT"/subdir/bye.txt
 mkdir "$ROOT"/subdir/subsubdir
 echo "__ciao__" > "$ROOT"/subdir/subsubdir/ciao.md
 mkdir "$ROOT"docs
-echo "latextest.feb-dev.net" > "$ROOT"/docs/CNAME
+echo "$CUSTOM_URL" > "$ROOT"/docs/CNAME
 echo "<h1>Hallo</h1><p>no content</p>" > "$ROOT"/docs/index.html
