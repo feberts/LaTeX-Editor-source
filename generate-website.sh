cb9fc9b1b3e2d82f0a6b6f87da8ee26ddcbffadd
feberts <64541491+feberts@users.noreply.github.com>
2023-08-11 13:08:33 +0200

    update

--- generate-website.sh
+++ generate-website.sh
@@ -7,3 +7,6 @@ mkdir "$ROOT"subdir
 echo "bye abc" > "$ROOT"/subdir/bye.txt
 mkdir "$ROOT"/subdir/subsubdir
 echo "__ciao__" > "$ROOT"/subdir/subsubdir/ciao.md
+mkdir "$ROOT"docs
+echo "latextest.feb-dev.net" > "$ROOT"/CNAME
+echo "<h1>Hallo</h1><p>no content</p>" > "$ROOT"/index.html
