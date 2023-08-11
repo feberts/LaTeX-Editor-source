21cce8ed2a937024e782aa4c9613a50478856a0e
feberts <64541491+feberts@users.noreply.github.com>
2023-08-11 13:42:26 +0200

    update

--- generate-website.sh
+++ generate-website.sh
@@ -15,7 +15,7 @@ echo "__ciao__" > "$ROOT"/subdir/subsubdir/ciao.md
 mkdir "$ROOT"docs
 echo "<h1>Hallo</h1><p>no content</p>" > "$ROOT"/docs/index.html
 
-if [ -n "$var" ]
+if [ -n "$CUSTOM_URL" ]
 then
     echo "$CUSTOM_URL" > "$ROOT"/docs/CNAME
 else
