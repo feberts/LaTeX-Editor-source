92a46b309ee1cee7afe50de51a25dfcee5064e77
feberts <64541491+feberts@users.noreply.github.com>
2023-08-13 12:11:16 +0200

    update

---
+++ web/editor.js
@@ -0,0 +1,187 @@
+// =============================================================================
+//
+//          FILE: editor.js
+//
+//         USAGE: This script is loaded with the html for the latex editor.
+//
+//   DESCRIPTION: This file provides functions that load the swiftlatex engine,
+//                initialize the code editor, handle user input, compile the
+//                latex document and display the pdf.
+//
+//       OPTIONS: ---
+//  REQUIREMENTS: ---
+//          BUGS: ---
+//         NOTES: ---
+//        AUTHOR: Fabian Eberts
+//  ORGANIZATION: ---
+//       CREATED: ---
+//      REVISION: ---
+//
+// =============================================================================
+
+// -----------------------------------------------------------------------------
+//  element access
+// -----------------------------------------------------------------------------
+
+const compile_button = document.getElementById("compilebutton");
+const tex_console = document.getElementById("texconsole");
+const console_output = document.getElementById("texconsoleoutput");
+const pdfviewer = document.getElementById("pdfviewer");
+const templatename = document.getElementById("templatename");
+const editor = ace.edit("editor");
+
+// -----------------------------------------------------------------------------
+//  initialize frontend
+// -----------------------------------------------------------------------------
+
+/*
+ * initialize html
+ */
+async function initialize_html()
+{
+    templatename.innerHTML = '„' + template_name + '“';
+}
+
+/*
+ * initialize the editor
+ */
+async function initialize_editor()
+{
+    // basic setup:
+    editor.setTheme("ace/theme/monokai");
+    editor.session.setMode("ace/mode/latex");
+    editor.session.setUseWrapMode(true);
+    editor.setFontSize(16);
+    editor.setShowPrintMargin(false);
+
+    // add text from latex main file:
+    const response = await fetch(main_tex_file);
+    const text = await response.text();
+    await editor.setValue(text);
+    await editor.clearSelection();
+}
+
+// -----------------------------------------------------------------------------
+//  swiftlatex
+// -----------------------------------------------------------------------------
+
+const engine = new PdfTeXEngine();
+
+/*
+ * add latex files as specified in the config file to the swiftlatex engine
+ */
+async function add_projectfiles_to_engine()
+{
+    console.log("add files to engine:");
+
+    for(const file of files)
+    {
+        await add_file(file);
+    }
+
+    await engine.setEngineMainFile(main_tex_file);
+    console.log(main_tex_file + " set as main file");
+}
+
+/*
+ * generic function to add files to the engine
+ */
+async function add_file(filename)
+{
+    if(/^.+\.(tex|bib|sty|cls)$/.test(filename))
+    {
+        await add_text(filename);
+        console.log(filename + " added as text");
+    }
+    else
+    {
+        await add_image(filename);
+        console.log(filename + " added as image");
+    }
+}
+
+/*
+ * add a text based file to the engine
+ */
+async function add_text(filename)
+{
+    let raw = await fetch(filename);
+    let text = await raw.text();
+    engine.writeMemFSFile(filename, text);
+}
+
+/*
+ * add an image file to the engine
+ */
+async function add_image(filename)
+{
+    let raw = await fetch(filename);
+    let blob = await raw.arrayBuffer();
+    engine.writeMemFSFile(filename, new Uint8Array(blob));
+}
+
+/*
+ * initialize the tex engine
+ */
+async function initialize_latex()
+{
+    await engine.loadEngine();
+    engine.setTexliveEndpoint("http://tex.feb-dev.net:4711/");
+    await add_projectfiles_to_engine();
+
+    // enable compile button:
+    compile_button.innerHTML = "Kompilieren";
+    compile_button.disabled = false;
+}
+
+/*
+ * compile document and display pdf
+ */
+async function compile()
+{
+    if(!engine.isReady())
+    {
+        console.log("engine is not ready");
+        return;
+    }
+
+    // disable compile button during compilation:
+    compile_button.disabled = true;
+    compile_button.innerHTML = "Kompiliere ...";
+
+    // pass editor text to engine, then compile:
+    engine.writeMemFSFile(main_tex_file, editor.getValue());
+    let result = await engine.compileLaTeX();
+
+    // display compile run console output:
+    console_output.innerHTML = result.log;
+    tex_console.style.display = "block"; // make element visible
+
+    // make compile button visible again:
+    compile_button.innerHTML = "Kompilieren";
+    compile_button.disabled = false;
+
+    // if compilation successful, display pdf:
+    if(result.status === 0)
+    {
+        const pdfblob = new Blob([result.pdf], { type : 'application/pdf' });
+        const objectURL = URL.createObjectURL(pdfblob);
+        setTimeout(()=>{ URL.revokeObjectURL(objectURL); }, 30000);
+        console.log(objectURL);
+        pdfviewer.innerHTML = `<embed src="${objectURL}" id="pdfviewerinner" type="application/pdf">`;
+    }
+}
+
+// -----------------------------------------------------------------------------
+//  initialization
+// -----------------------------------------------------------------------------
+
+/*
+ * initialize all components; this function gets called, when the html is loaded
+ */
+async function init()
+{
+    await initialize_html();
+    await initialize_editor();
+    await initialize_latex();
+}
